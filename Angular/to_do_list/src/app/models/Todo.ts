d0a445756ad87e330bd946ead2d503a5fc28af66
Sidharth Pandey <sidp919@gmail.com>
2021-12-19 22:08:00 +0530

    4th-Major-commit: using *ngFor directive to display todosArr

---
+++ Angular/to_do_list/src/app/models/Todo.ts
@@ -0,0 +1,6 @@
+export class Todo{
+    srNo: number = 0
+    title: string = ""
+    desc: string = ""
+    active: boolean = false
+}
--- Angular/to_do_list/src/app/subComponents/to-do-list/to-do-list.component.ts
+++ Angular/to_do_list/src/app/subComponents/to-do-list/to-do-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
 
 @Component({
   selector: 'app-to-do-list',
@@ -7,7 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToDoListComponent implements OnInit {
 
-  constructor() { }
+  todosArr: Todo[];
+  constructor() { 
+    this.todosArr = [
+      {
+        srNo: 1,
+        title: "First title",
+        desc: "description of first todo",
+        active: true
+      },
+      {
+        srNo: 2,
+        title: "Second title",
+        desc: "description of second todo",
+        active: true
+      },
+      {
+        srNo: 3,
+        title: "Third title",
+        desc: "description of third todo",
+        active: true
+      },
+      {
+        srNo: 4,
+        title: "Fourth title",
+        desc: "description of fourth todo",
+        active: true
+      },
+      {
+        srNo: 5,
+        title: "Fifth title",
+        desc: "description of fifth todo",
+        active: true
+      }
+    ]
+  }
 
   ngOnInit(): void {
   }
