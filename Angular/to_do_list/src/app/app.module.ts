082a7f5d44d660531c5f8d37d15fde4ea45599a2
Sidharth Pandey <sidp919@gmail.com>
2021-12-19 19:30:29 +0530

    3rd-Major-commit: Created new subComponent to-do-list

--- Angular/to_do_list/src/app/app.module.ts
+++ Angular/to_do_list/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToDoListComponent } from './subComponents/to-do-list/to-do-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoListComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angular/to_do_list/src/app/subComponents/to-do-list/to-do-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-list',
+  templateUrl: './to-do-list.component.html',
+  styleUrls: ['./to-do-list.component.css']
+})
+export class ToDoListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
