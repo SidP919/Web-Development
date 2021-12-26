10bdb380812e2434f691ec4c3ab5cccf7a5d9934
Sidharth Pandey <sidp919@gmail.com>
2021-12-26 21:29:58 +0530

    8th-Major-commit: Added Form-Validation functionality

--- Angular/to_do_list/src/app/subComponents/add-to-do-item/add-to-do-item.component.ts
+++ Angular/to_do_list/src/app/subComponents/add-to-do-item/add-to-do-item.component.ts
@@ -19,12 +19,40 @@ export class AddToDoItemComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.validateAddTodoForm();
+  }
+
+  validateAddTodoForm(): void {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.form-validate')
+    
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event: { preventDefault: () => void; stopPropagation: () => void; }) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+    });
   }
 
   //Emit task details to to-do-list component:
   onSubmit(){
-    this.todo.title=this.taskTitle;
-    this.todo.desc=this.taskDescription;
-    this.addToDo.emit(this.todo);
+
+    if(this.taskTitle!== "" && this.taskDescription!== ""){
+      this.todo.title=this.taskTitle;
+      this.todo.desc=this.taskDescription;
+      this.addToDo.emit(this.todo);
+      this.taskTitle="";
+      this.taskDescription="";
+    }
+    else{
+      console.log("Add Todo Form is not filled with valid details.");
+    }
   }
+
 }
